01c93db4d762a5d7db72dc59facff9532b6d203e
22fariz22 <fariz08>
2022-07-19 22:30:58 +0300

    add in model showAllTasks functtion. Add index.html where will all fields Task struct

--- model.go
+++ model.go
@@ -1,16 +1,36 @@
 package main
 
-import "time"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"time"
+)
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println(err)
+	}
+}
 
 type Task struct {
-	id    int
-	time  time.Time
-	title string
+	Id    int
+	Time  time.Time
+	Title string
+	Body  string
 }
+
 type AllTasks struct {
 	Tasks []*Task
 }
 
 func ShowAllTasks() (at *AllTasks) {
-	return
+	file, err := os.OpenFile("list.json", os.O_RDWR|os.O_APPEND, 0666)
+	checkError(err)
+	b, err := ioutil.ReadAll(file)
+	var alTasks AllTasks
+	json.Unmarshal(b, &alTasks.Tasks)
+	checkError(err)
+	return &alTasks
 }
